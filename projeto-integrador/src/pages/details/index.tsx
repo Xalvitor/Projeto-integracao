e2d22262929d903602aac1e129ee380a8c565ccc
Xalvitor <xalvitor@gmail.com>
2024-10-02 18:24:48 -0300

    Ajustes

--- projeto-integrador/src/pages/details/index.tsx
+++ projeto-integrador/src/pages/details/index.tsx
@@ -6,6 +6,7 @@ import { getApiProductById } from "./service";
 import { useEffect, useState } from "react";
 import { Product } from "./types";
 import ProductLoading from "../../components/product-loading";
+import { EasyZoomOnHover } from "easy-magnify";
 export default function Details(){
     const toastId = "custom-id-yes"
 
@@ -38,10 +39,11 @@ export default function Details(){
 
     return(
         <UserTemplate>
+
             <ToastContainer/ >
             {isloadingProducts &&< ProductLoading />}
 
-            <h1>Details</h1>
+            <h1>Detalhes do produto</h1>
             <p className="text-[30px]">{product.name}</p>
             <div className="flex mt-10 gap-10 justify-center">
                 <div className="w-[40%]">
@@ -57,11 +59,11 @@ export default function Details(){
 
                 <div className="shadow-sm bg-white px-10 py-2">
                     <p>Informações do vendedor</p>
-                    <p>{product.user.name}</p>
-                    <p>{product.user.city}</p>
-                    <p>{product.user.state}</p>
-                    <p>{product.user.email}</p>
-                    <p>{product.user.phone}</p>
+                    <p>{product.user?.name}</p>
+                    <p>{product.user?.city}</p>
+                    <p>{product.user?.state}</p>
+                    <p>{product.user?.email}</p>
+                    <p>{product.user?.phone}</p>
                 </div>
 
                 <div className="shadow-sm bg-white px-10 py-2">
